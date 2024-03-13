901917a985042dfdc5cec03206f975ca74f5e8a7
Jiwon <tj1tj2tj3@naver.com>
2024-03-14 00:48:48 +0900

    [style] css수정

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -33,19 +33,19 @@ const reportPage = async () => {
         className='bg-rose-100 border-rose-400 mt-4 p-3 rounded-md border-solid border
         w-1/3 min-h-16 flex items-center justify-center'
       >
-        <div className='mb-2'>현재까지 총 {todos.length}개의 할일이 등록되었습니다.</div>
+        <div>현재까지 총 {todos.length}개의 할일이 등록되었습니다.</div>
       </div>
       <div
         className='bg-amber-100 border-amber-400 mt-4 p-3 rounded-md border-solid border
         w-1/3 min-h-16 flex items-center justify-center'
       >
-        <div className='mb-2'>미완료 된 항목은 {InProgressTodos.length}개 입니다.</div>
+        <div>미완료 된 항목은 {InProgressTodos.length}개 입니다.</div>
       </div>
       <div
         className='bg-lime-100 border-lime-400 mt-4 p-3 rounded-md border-solid border
         w-1/3 min-h-16 flex items-center justify-center'
       >
-        <div className='mb-2'>완료 된 항목은 {completedTodos.length}개 입니다.</div>
+        <div>완료 된 항목은 {completedTodos.length}개 입니다.</div>
       </div>
       <Link href='/todo-csr' className='mt-5 decoration-solid underline'>
         할일 추가하러 가기 (*ᴗ͈ˬᴗ͈)ꕤ*.ﾟ
